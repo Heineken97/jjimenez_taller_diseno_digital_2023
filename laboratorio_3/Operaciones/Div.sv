96a57d358f53183abf408b5686de425102da72ee
Joseph Jimenez <josephdjz@hotmail.com>
2023-03-29 00:51:58 -0600

    [UPDATE] Div
    Actualizando división quitando el modulo dentro

--- laboratorio_3/Operaciones/Div.sv
+++ laboratorio_3/Operaciones/Div.sv
@@ -1,12 +1,10 @@
 module Div #(parameter N=8)(
 	input logic signed [N-1:0] dividendo,
 	input logic signed [N-1:0] divisor,
-	output logic signed [N-1:0] cociente,
-	output logic signed [N-1:0] residuo
+	output logic signed [N-1:0] cociente
 );
 always_comb begin
 	cociente = dividendo/ divisor;
-	residuo = dividendo % divisor;
 end
 
 endmodule
