094a08d637c36f74e062897af0761400b81a4317
Joseph Jimenez <josephdjz@hotmail.com>
2023-05-02 13:47:18 -0600

    [UPDATE] VGA
    se puede dibujar en la posicion deseada

--- laboratorio_4/VGA/Memory_VGA.sv
+++ laboratorio_4/VGA/Memory_VGA.sv
@@ -1,15 +1,31 @@
 module Memory_VGA(
-	input logic [7:0] img_selected,
-	input logic [7:0] yoff, xoff,
-	output logic pixel);
-	
-	logic [119:0] charrom[2047:0]; 
-	logic [119:0] line;
-	
-	initial
-	$readmemb("img_VGA.txt", charrom);
-	
-	assign line = charrom[yoff];
-	assign pixel = line[7'd119-xoff];
-
+    input logic [7:0] img_selected,
+    input logic [7:0] yoff, xoff,
+    input logic [11:0] memorySelect,
+    output logic pixel
+);
+    logic [119:0] charrom[2047:0]; 
+    logic [119:0] line;
+    
+    always @* begin
+        case(memorySelect)
+            0: $readmemb("img_VGA_0.txt", charrom);//background
+            2: $readmemb("img_VGA_1.txt", charrom);//2
+            4: $readmemb("img_VGA_2.txt", charrom);//4
+            8: $readmemb("img_VGA_3.txt", charrom);//8
+            16: $readmemb("img_VGA_4.txt", charrom);//16
+            32: $readmemb("img_VGA_5.txt", charrom);//32
+            64: $readmemb("img_VGA_6.txt", charrom);//64
+            128: $readmemb("img_VGA_7.txt", charrom);//128
+            256: $readmemb("img_VGA_8.txt", charrom);//256
+            512: $readmemb("img_VGA_9.txt", charrom);//512
+            1024: $readmemb("img_VGA_10.txt", charrom);//1024
+            2048: $readmemb("img_VGA_11.txt", charrom);//2048
+            default: $readmemb("img_VGA_0.txt", charrom);//background
+        endcase
+		  line = charrom[yoff];
+		  pixel = line[7'd119-xoff];
+    end
+	 
 endmodule
+
--- laboratorio_4/VGA/Paint_VGA.sv
+++ laboratorio_4/VGA/Paint_VGA.sv
@@ -1,3 +1,4 @@
+
 module Paint_VGA(
 	input logic [9:0] x, y,
 	output logic [7:0] Red, Green, Blue);
@@ -7,26 +8,180 @@ module Paint_VGA(
 	logic [9:0] board_limit = 10'd550;
 	//logic [7:0] img_selected = y[8:3]; 
 	
+	reg  initX = 0, initY = 0;
 	logic [7:0] w_red, w_green, w_blue;
-	logic paint,inLine,inSquare, pixel_0,pixel_1,pixel_2,pixel_3,pixel_4,pixel_5,pixel_6,pixel_7,pixel_8,pixel_9,pixel_10,pixel_11;
+	logic square_0,paint,inLine,inSquare, pixel_0,pixel_1,pixel_2,pixel_3,pixel_4,pixel_5,pixel_6,pixel_7,pixel_8,pixel_9,pixel_10,pixel_11,pixel_13,pixel_14,pixel_15;
 	
-	Memory_VGA Memory(y[8:3], x[6:0], y[6:0], pixel);
-
+	Memory_VGA Memory_0(.img_selected(y[8:3]), .yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_0), .memorySelect(2));
+	Memory_VGA Memory_1(.img_selected(y[8:3]), .yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_1), .memorySelect(4));
+	Memory_VGA Memory_2(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_2), .memorySelect(2048));
+	Memory_VGA Memory_3(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_3), .memorySelect(8));
+	Memory_VGA Memory_4(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_4), .memorySelect(16));
+	Memory_VGA Memory_5(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_5), .memorySelect(32));
+	Memory_VGA Memory_6(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_6), .memorySelect(64));
+	Memory_VGA Memory_7(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_7), .memorySelect(128));
+	Memory_VGA Memory_8(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_8), .memorySelect(256));
+	Memory_VGA Memory_9(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_9), .memorySelect(512));
+	Memory_VGA Memory_10(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_10), .memorySelect(1024));
+	Memory_VGA Memory_11(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_11), .memorySelect(2048));
+	Memory_VGA Memory_12(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_12), .memorySelect(8));
+	Memory_VGA Memory_13(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_13), .memorySelect(16));
+	Memory_VGA Memory_14(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_14), .memorySelect(8));
+	Memory_VGA Memory_15(.img_selected(y[8:3]),.yoff(x[6:0]), .xoff(y[6:0]),.pixel(pixel_15), .memorySelect(4));
+	
 	SquareGen_VGA MatrixBackground(.x(x),.y(y),.initX(initXMatrix),.initY(initYMatrix),.n(N_side),.inSquare(inSquare));
 	
 	LineGen_VGA MatrixLines(.x(x), .y(y), .initX(initXMatrix), .initY(initYMatrix), .n(N_side), .inLine(inLine));
 	
 	always_comb begin
-		if (x[8:7] == 2'b10 && y[8:7] == 2'b01 && pixel== 1'b1) begin
+	
+		if ((initX+1 <= x && x < initX+119 && initY+1 <= y && y < initX+119) && pixel_0== 1'b1) begin
 			w_red   <= 8'hFF;
 			w_blue  <= 8'hFF; 
 			w_green <= 8'hFF;
-		end else if (x[8:7] == 2'b10 && y[8:7] == 2'b01 && pixel== 1'b0) begin 
+		end else if ((initX+1 <= x && x < initX+119 && initY+1 <= y && y < initY+119) && pixel_0== 1'b0) begin 
 			w_red   <= 8'd148;
 			w_blue  <= 8'd26; 
 			w_green <= 8'd28;
-		end else if (inSquare && inLine) begin
 			
+		end else if ((initX+121 <= x && x < initX+239 && initY+1 <= y && y < initY+119) && pixel_1== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+121 <= x && x < initX+239 && initY+1 <= y && y < initY+119) && pixel_1== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+241 <= x && x < initX+359 && initY+1 <= y && y < initY+119) && pixel_2== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+241 <= x && x < initX+359 && initY+1 <= y && y < initY+119) && pixel_2== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+361 <= x && x < initX+479 && initY+1 <= y && y < initY+119) && pixel_3== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+361 <= x && x < initX+479 && initY+1 <= y && y < initY+119) && pixel_3== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+1 <= x && x < initX+119 && initY+121 <= y && y < initY+239)  && pixel_4== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+1 <= x && x < initX+119  && initY+121 <= y && y < initY+239) && pixel_4== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+121 <= x && x < initX+239 && initY+121 <= y && y < initY+239)  && pixel_5== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+121 <= x && x < initX+239 && initY+121 <= y && y < initY+239) && pixel_5== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+241 <= x && x < initX+359 &&  initY+121 <= y && y < initY+239) && pixel_6== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+241 <= x && x < initX+359 &&  initY+121 <= y && y < initY+239) && pixel_6== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+361 <= x && x < initX+479 &&  initY+121 <= y && y < initY+239) && pixel_7== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+361 <= x && x < initX+479 &&  initY+121 <= y && y < initY+239) && pixel_7== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+1 <= x && x < initX+119 &&  initY+241 <= y && y < initY+359) && pixel_8== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+1 <= x && x < initX+119 &&  initY+241 <= y && y < initY+359) && pixel_8== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+			
+		end else if ((initX+121 <= x && x < initX+239 && initY+241 <= y && y < initY+359) && pixel_9== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+121 <= x && x < initX+239 && initY+241 <= y && y < initY+359) && pixel_9== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+241 <= x && x < initX+359 && initY+241 <= y && y < initY+359)  && pixel_10== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+241 <= x && x < initX+359 && initY+241 <= y && y < initY+359)  && pixel_10== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+361 <= x && x < initX+479 && initY+241 <= y && y < initY+359)  && pixel_11== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+361 <= x && x < initX+479 && initY+241 <= y && y < initY+359) && pixel_11== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+
+			
+		end else if ((initX+1 <= x && x < initX+119 && initY+361 <= y && y < initY+479) && pixel_12== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+1 <= x && x < initX+119 && initY+361 <= y && y < initY+479)  && pixel_12== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+121 <= x && x < initX+239 && initY+361 <= y && y < initY+479) && pixel_13== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+121 <= x && x < initX+239 && initY+361 <= y && y < initY+479)  && pixel_13== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+241 <= x && x < initX+359 && initY+361 <= y && y < initY+479) && pixel_14== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+241 <= x && x < initX+359 && initY+361 <= y && y < initY+479)  && pixel_14== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+			
+		end else if ((initX+361 <= x && x < initX+479 && initY+361 <= y && y < initY+479) && pixel_15== 1'b1) begin
+			w_red   <= 8'hFF;
+			w_blue  <= 8'hFF; 
+			w_green <= 8'hFF;
+		end else if ((initX+361 <= x && x < initX+479 && initY+361 <= y && y < initY+479)  && pixel_15== 1'b0) begin 
+			w_red   <= 8'd148;
+			w_blue  <= 8'd26; 
+			w_green <= 8'd28;
+
+		end else if (inSquare && inLine) begin			
 			w_red   <= 8'd243;
 			w_green <= 8'd114;
 			w_blue  <= 8'd32;
