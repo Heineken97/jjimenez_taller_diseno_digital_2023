0ef32c93a228bdff1b736c5f9778ea6780d31524
Carlos12001 <carlos.andres12001@gmail.com>
2023-06-11 20:34:42 -0600

    feat: testting

--- Proyecto_Final/Ecualizacion_de_histogramas/src/arm_processor/imem.sv
+++ Proyecto_Final/Ecualizacion_de_histogramas/src/arm_processor/imem.sv
@@ -6,7 +6,7 @@ module imem(
 	logic [31:0] RAM[63:0];
 	
 	initial
-		$readmemh("C:/Users/Odin/Desktop/Proyecto_Final/Ecualizacion_de_histogramas/src/arm_processor/memfile.dat", RAM);
+		$readmemh("C:/Users/carlo/Desktop/random/jjimenez_taller_diseno_digital_2023/Proyecto_Final/Ecualizacion_de_histogramas/src/arm_processor/memfile.dat", RAM);
 		
 	assign ReadData = RAM[Address[31:2]];
 	
--- Proyecto_Final/Ecualizacion_de_histogramas/src/top_module.sv
+++ Proyecto_Final/Ecualizacion_de_histogramas/src/top_module.sv
@@ -3,9 +3,7 @@ module top_module(
 	input 	logic 			rst ,
 	output 	logic [31:0] 	WriteData,
 	output 	logic [31:0] 	DataAddr,
-	output 	logic 			MemWrite,output logic CLK_VGA, 
-   output 	logic 			SYNC_H, SYNC_V, SYNC_B, SYNC_BLANK, 
-   output	logic [7:0] 	Red, Green, Blue
+	output 	logic 			MemWrite
 	);
 
 	logic [31:0] ReadData, PC, Instruction;
@@ -16,7 +14,5 @@ module top_module(
 	imem instruction_memory(.Address(PC), .ReadData(Instruction));
 
 	dmem data_memory(.clk(clk),.WriteEnable(MemWrite),.Address(DataAddr),.WriteData(WriteData),.ReadData(ReadData));
-	
-	controller_VGA controllerVGA(.clk(clk),.rst(rst), .CLK_VGA(CLK_VGA),.SYNC_H(SYNC_H), .SYNC_V(SYNC_V), .SYNC_B(SYNC_B), .SYNC_BLANK(SYNC_BLANK),.Red(Red), .Green(Green), .Blue(Blue));
 							
 endmodule
